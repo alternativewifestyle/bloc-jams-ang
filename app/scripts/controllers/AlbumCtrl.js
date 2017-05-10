26655e875012e1979da09164484f50576b93c76d
Cassie Smith <marywasadj@gmail.com>
2017-05-09 19:04:28 -0700

    checkpoint 5 album controller

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,14 +1,27 @@
 //Add an albumData property that holds a copy of albumPicasso.
-//fuck if I know, I can't tell if it works or not. 
+//fuck if I know, I can't tell if it works or not.
+
+// var albumData = {
+//   title: 'Цвета',
+//   artist: 'Пабло Пикассо',
+//   label: 'Кубизм',
+//   year: '1881',
+//   albumArtUrl: 'assets/images/album_covers/01.png',
+//   songs: [   { title: 'синий', duration: 161.71, audioUrl: 'assets/music/blue' },
+//          { title: 'зеленый', duration: 103.96, audioUrl: 'assets/music/green' },
+//          { title: 'красный', duration: 268.45, audioUrl: 'assets/music/red' },
+//          { title: 'розовый', duration: 153.14, audioUrl: 'assets/music/pink' },
+//          { title: 'фуксин', duration: 374.22, audioUrl: 'assets/music/magenta' }
+//    ]
+// };
+
 
 (function() {
   function AlbumCtrl() {
-this.albums = [];
-for (var i=0; i<12; i++){
-  this.albums.push(angular.copy(albumPicasso));
-}
+  	this.albumData = albumPicasso;
   }
+
   angular
-  .module('blocJams')
-  .controller('AlbumCtrl', AlbumCtrl);
+    .module('blocJams')
+    .controller('AlbumCtrl', AlbumCtrl);
 })();
