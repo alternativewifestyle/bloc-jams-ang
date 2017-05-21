929e7301c34959a1eae22562f6dc9cd0203288ad
Cassie Smith <marywasadj@gmail.com>
2017-05-20 19:58:25 -0700

    checkpoint 10

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-  function SongPlayer(Fixtures) {
+  function SongPlayer($rootScope, Fixtures) {
          var SongPlayer = {};
 
          var currentAlbum = Fixtures.getAlbum();
@@ -27,6 +27,12 @@
                 preload: true
             });
 
+            currentBuzzObject.bind('timeupdate', function() {
+       $rootScope.$apply(function() {
+           SongPlayer.currentTime = currentBuzzObject.getTime();
+       });
+   });
+
             SongPlayer.currentSong = song;
          };
 
@@ -45,6 +51,12 @@
          * @type {Object}
          */
 SongPlayer.currentSong = null;
+
+/**
+* @desc Current playback time (in seconds) of currently playing song
+* @type {Number}
+*/
+SongPlayer.currentTime = null;
 
          var playSong = function(song) {
           currentBuzzObject.play();
@@ -96,11 +108,21 @@ SongPlayer.next = function() {
            playSong(song);
        }
 };
+/**
+ * @function setCurrentTime
+ * @desc Set current time (in seconds) of currently playing song
+ * @param {Number} time
+ */
+ SongPlayer.setCurrentTime = function(time) {
+     if (currentBuzzObject) {
+         currentBuzzObject.setTime(time);
+     }
+ };
 
          return SongPlayer;
     }
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', ['Fixtures', SongPlayer]);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
